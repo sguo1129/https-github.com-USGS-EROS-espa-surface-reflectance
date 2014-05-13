a1882c5d4b65548bce28cead6e0dd9ab4c6887f3
Ronald Dilley <rdilley@usgs.gov>
2014-05-13 15:12:28 +0000

    enhanced
    git-svn-id: https://edclxs131.cr.usgs.gov:18080/svn/lsrd/l8_sr/trunk@791 48de96a7-6a0d-487c-b84a-40161698abc1

--- not-validated-prototype-l8_sr/processldcm.sh
+++ not-validated-prototype-l8_sr/processldcm.sh
@@ -3,31 +3,32 @@
 rootdir=$1
 case=$2
 mtlfile=`ls $rootdir$case/*.txt`
-echo 0 >ldcm.inp
-ls $rootdir$case/*_B?.TIF >>ldcm.inp
-ls $rootdir$case/*_B1?.TIF >>ldcm.inp
-ls $rootdir$case/*_BQA.TIF >>ldcm.inp
+inpfile=$case\.input
+echo 0 >$inpfile
+ls $rootdir$case/*_B?.TIF >>$inpfile
+ls $rootdir$case/*_B1?.TIF >>$inpfile
+ls $rootdir$case/*_BQA.TIF >>$inpfile
 yearday=`echo $case | cut -c 10-16`
 echo $yearday
 fileanc=`ls LANDSATANC/L8ANC$yearday.hdf_fused`
-echo $fileanc >>ldcm.inp
+echo $fileanc >>$inpfile
 mtlfile=`ls $rootdir$case/*.txt`
 rnl=`grep REFLECTIVE_LINES $mtlfile | awk '{print $3}'`
 rnc=`grep REFLECTIVE_SAMPLES $mtlfile | awk '{print $3}'`
 pnl=`grep  PANCHROMATIC_LINES $mtlfile | awk '{print $3}'`
 pnc=`grep PANCHROMATIC_SAMPLES $mtlfile | awk '{print $3}'`
-echo $rnl $rnc $pnl $pnc >>ldcm.inp
+echo $rnl $rnc $pnl $pnc >>$inpfile
 ts=`grep SUN_ELEVATION $mtlfile | awk '{print 90.-$3}'`
 fs=`grep SUN_AZIMUTH $mtlfile | awk '{print $3}'`
-echo $ts $fs >>ldcm.inp
+echo $ts $fs >>$inpfile
 utmzone=`grep  "UTM_ZONE" $mtlfile | awk '{print $3}'`
 x0=`grep  "CORNER_UL_PROJECTION_X_PRODUCT" $mtlfile | awk '{print $3}'`
 y0=`grep  "CORNER_UL_PROJECTION_Y_PRODUCT" $mtlfile | awk '{print $3}'`
-echo $utmzone 1 1 $y0 $x0 >>ldcm.inp
+echo $utmzone 1 1 $y0 $x0 >>$inpfile
 #exit
 
-#LDCMSR-v1.0 <ldcm.inp >$case.log
-#LDCMSR-v1.0 <ldcm.inp 
+#LDCMSR-v1.0 <$inpfile >$case.log
+#LDCMSR-v1.0 <$inpfile
 #mv correcteddata.hdf sr$case.hdf
 
 
