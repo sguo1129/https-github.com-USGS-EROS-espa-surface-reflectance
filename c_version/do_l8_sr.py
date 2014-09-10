dd1014c211213d62d0cd0cb09a85e45045372610
Gail Schmidt <gschmidt@usgs.gov>
2014-09-10 20:06:50 +0000

    Updated the scripts to use Python logging libraries.
    git-svn-id: https://edclxs131.cr.usgs.gov:18080/svn/lsrd/l8_sr/trunk@872 48de96a7-6a0d-487c-b84a-40161698abc1

--- c_version/do_l8_sr.py
+++ c_version/do_l8_sr.py
@@ -5,27 +5,10 @@ import re
 import commands
 import datetime
 from optparse import OptionParser
+import logging
 
 ERROR = 1
 SUCCESS = 0
-
-############################################################################
-# Description: logIt logs the information to the logfile (if valid) or to
-# stdout if the logfile is None.
-#
-# Inputs:
-#   msg - message to be printed/logged
-#   log_handler - log file handler; if None then print to stdout
-#
-# Returns: nothing
-#
-# Notes:
-############################################################################
-def logIt (msg, log_handler):
-    if log_handler == None:
-        print msg
-    else:
-        log_handler.write (msg + '\n')
 
 
 #############################################################################
@@ -36,8 +19,8 @@ def logIt (msg, log_handler):
 # input file.
 #
 # History:
-#   Updated on {date} by {developer}, USGS/EROS
-#   {description}
+#   Updated on 9/9/2014 by Gail Schmidt, USGS/EROS
+#   Use standard python logging instead of a user-defined log file
 #
 # Usage: do_l8_sr.py --help prints the help message
 ############################################################################
@@ -49,7 +32,7 @@ class SurfaceReflectance():
 
     ########################################################################
     # Description: runSr will use the parameters passed for the input/output
-    # files, logfile, and usebin.  If input/output files are None (i.e. not
+    # files, and usebin.  If input/output files are None (i.e. not
     # specified) then the command-line parameters will be parsed for this
     # information.  The surface reflectance application is then executed to
     # generate the desired outputs on the specified input file.  If a log
@@ -62,8 +45,6 @@ class SurfaceReflectance():
     #       should be completed.  True or False.  Default is True, otherwise
     #       the processing will halt after the TOA reflectance products are
     #       complete.
-    #   logfile - name of the logfile for logging information; if None then
-    #       the output will be written to stdout
     #   usebin - this specifies if the surface reflectance exe resides in the
     #       $BIN directory; if None then the surface reflectance exe is
     #       expected to be in the PATH
@@ -82,7 +63,7 @@ class SurfaceReflectance():
     #      the parameters will be pulled from the command line.
     #######################################################################
     def runSr (self, xml_infile=None, process_sr=None, write_toa=False, \
-        logfile=None, usebin=None):
+        usebin=None):
         # if no parameters were passed then get the info from the
         # command line
         if xml_infile == None:
@@ -104,13 +85,10 @@ class SurfaceReflectance():
                 action="store_true",
                 help="use BIN environment variable as the location of " + \
                      "surface reflectance application")
-            parser.add_option ("-l", "--logfile", type="string", dest="logfile",
-                help="name of optional log file", metavar="FILE")
             (options, args) = parser.parse_args()
     
             # validate the command-line options
             usebin = options.usebin          # should $BIN directory be used
-            logfile = options.logfile        # name of the log file
 
             # XML input file
             xml_infile = options.xml
@@ -122,12 +100,10 @@ class SurfaceReflectance():
             process_sr = options.process_sr
             write_toa = options.write_toa
 
-        # open the log file if it exists; use line buffering for the output
-        log_handler = None
-        if logfile != None:
-            log_handler = open (logfile, 'w', buffering=1)
+        # get the logger
+        logger = logging.getLogger(__name__)
         msg = 'Surface reflectance processing of Landsat file: %s' % xml_infile
-        logIt (msg, log_handler)
+        logger.info (msg)
         
         # should we expect the surface reflectance application to be in the
         # PATH or in the BIN directory?
@@ -136,24 +112,24 @@ class SurfaceReflectance():
             bin_dir = os.environ.get('BIN')
             bin_dir = bin_dir + '/'
             msg = 'BIN environment variable: %s' % bin_dir
-            logIt (msg, log_handler)
+            logger.info (msg)
         else:
             # don't use a path to the surface reflectance application
             bin_dir = ""
             msg = 'L8 surface reflectance executable expected to be in the PATH'
-            logIt (msg, log_handler)
+            logger.info (msg)
         
         # make sure the XML file exists
         if not os.path.isfile(xml_infile):
             msg = "Error: XML file does not exist or is not accessible: " + \
                 xml_infile
-            logIt (msg, log_handler)
+            logger.error (msg)
             return ERROR
 
         # use the base XML filename and not the full path.
         base_xmlfile = os.path.basename (xml_infile)
         msg = 'Processing XML file: %s' % base_xmlfile
-        logIt (msg, log_handler)
+        logger.info (msg)
         
         # get the path of the XML file and change directory to that location
         # for running this script.  save the current working directory for
@@ -166,18 +142,17 @@ class SurfaceReflectance():
         if not os.access(xmldir, os.W_OK):
             msg = 'Path of XML file is not writable: %s.  Script needs ' + \
                 'write access to the XML directory.' % xmldir
-            logIt (msg, log_handler)
+            logger.error (msg)
             return ERROR
         msg = 'Changing directories for surface reflectance processing: %s' % \
             xmldir
-        logIt (msg, log_handler)
+        logger.info (msg)
         os.chdir (xmldir)
 
         # pull the date from the XML filename to determine which auxiliary
         # file should be used for input.  Example: LC80410272013181LGN00.xml
         # uses L8ANC2013181.hdf_fused.
         aux_file = 'L8ANC' + base_xmlfile[9:16] + '.hdf_fused'
-        print 'DEBUG: auxiliary file: %s' % aux_file
 
         # run surface reflectance algorithm, checking the return status.  exit
         # if any errors occur.
@@ -192,25 +167,31 @@ class SurfaceReflectance():
         cmdstr = "%sl8_sr --xml=%s --aux=%s %s%s--verbose" % \
             (bin_dir, xml_infile, aux_file, process_sr_opt_str, \
              write_toa_opt_str)
-        print 'DEBUG: l8_sr command: %s' % cmdstr
+        msg = 'Executing l8_sr command: %s' % cmdstr
+        logger.info (msg)
         (status, output) = commands.getstatusoutput (cmdstr)
-        logIt (output, log_handler)
+        logger.info (output)
         exit_code = status >> 8
         if exit_code != 0:
             msg = 'Error running l8_sr.  Processing will terminate.'
-            logIt (msg, log_handler)
+            logger.error (msg)
             os.chdir (mydir)
             return ERROR
         
         # successful completion.  return to the original directory.
         os.chdir (mydir)
         msg = 'Completion of surface reflectance.'
-        logIt (msg, log_handler)
-        if logfile != None:
-            log_handler.close()
+        logger.info (msg)
         return SUCCESS
 
 ######end of SurfaceReflectance class######
 
 if __name__ == "__main__":
+    # setup the default logger format and level. log to STDOUT.
+    logging.basicConfig(format=('%(asctime)s.%(msecs)03d %(process)d'
+                                ' %(levelname)-8s'
+                                ' %(filename)s:%(lineno)d:'
+                                '%(funcName)s -- %(message)s'),
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
     sys.exit (SurfaceReflectance().runSr())
