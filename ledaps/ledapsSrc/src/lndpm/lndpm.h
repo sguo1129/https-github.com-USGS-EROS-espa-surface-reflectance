08e6a3ac4f5de2023632d865dccb721441ef1097
Gail Schmidt <gschmidt@usgs.gov>
2015-04-01 21:59:40 +0000

    Updated the version number for the lndcal changes.
    git-svn-id: https://ledaps.googlecode.com/svn/trunk@220 10324028-1983-28d5-8f14-fde5f4c4070c

--- ledaps/ledapsSrc/src/lndpm/lndpm.h
+++ ledaps/ledapsSrc/src/lndpm/lndpm.h
@@ -32,7 +32,7 @@ NOTES:
 
 /* Defines */
 /* LEDAPS VERSION definitions */
-#define LEDAPS_VERSION "2.2.1"
+#define LEDAPS_VERSION "2.3"
 #define logFile "LogReport"
 
 /* define useful constants */
